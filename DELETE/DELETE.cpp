d6f05733292f8e51fa01a598df9d3ebf5074c68a
g.gonfiantini <gaetan.gonfiantini@gmail.com>
2023-05-23 08:59:36 +0200

    DELETE commenté

--- DELETE/DELETE.cpp
+++ DELETE/DELETE.cpp
@@ -28,22 +28,25 @@ using namespace std;
 /**
  * Cette fonction supprime un document de la collection avec son ID
  */
-
 void deleteOneDocument(mongocxx::client &client) {
     mongocxx::database db = client["actiaDataBase"];
+    //Demande à l'utilisateur le nom de la collection
     cout << "Entrer le nom de la collection : ";
     string collectionName;
-
     getline(cin, collectionName);
+    //Vérifie si la collection existe
     if(!collectionExist(db, collectionName)){
         cout << "La collection n'existe pas.\n";
         return;
     }
-    mongocxx::collection collection = db[collectionName];
+    mongocxx::collection collection = db[collectionName]; //Récupère la collection
+    //Demande à l'utilisateur l'ID du document à supprimer
     cout << "Entrer l'ID du document a supprimer : ";
     string id;
     getline(cin, id);
+    //Requête pour supprimer le document
     bsoncxx::stdx::optional<mongocxx::result::delete_result> result = collection.delete_one(document{} << "_id" << id << finalize);
+    //Vérifie si la suppression s'est bien passée
     if (result) {
         cout << "Document supprime avec succes.\n";
     } else {
@@ -56,20 +59,23 @@ void deleteOneDocument(mongocxx::client &client) {
  */
 
 void deleteAllDocuments(mongocxx::client &client) {
-    //Chrono
+    //Chronomètre pour calculer le temps d'exécution
     auto startChrono = chrono::high_resolution_clock::now();
     mongocxx::database db = client["actiaDataBase"];
+    //Demande à l'utilisateur le nom de la collection à vider
     cout << "Entrer le nom de la collection : ";
     string collectionName;
     getline(cin, collectionName);
+    //Vérifie si la collection existe
     if(!collectionExist(db, collectionName)){
         cout << "La collection n'existe pas.\n";
         return;
     }
     mongocxx::collection collection = db[collectionName];
+    //Requête pour supprimer tous les documents de la collection
     collection.delete_many({});
     cout << "Tous les documents ont ete supprimes avec succes.\n";
-    //Chrono fin
+    //Chronomètre fin
     auto endChrono = chrono::high_resolution_clock::now();
     chrono::duration<double> elapsed = endChrono - startChrono;
     cout << "Temps d'execution : " << elapsed.count() << " secondes\n";
